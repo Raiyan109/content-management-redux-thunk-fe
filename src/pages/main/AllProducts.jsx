1c3ccbad0ce095a394ce51d75963599957d29511
Raiyan109 <kraiyan109@gmail.com>
2023-11-06 15:49:44 +0600

    reading history added

--- src/pages/main/AllProducts.jsx
+++ src/pages/main/AllProducts.jsx
@@ -13,15 +13,15 @@ const AllProducts = () => {
 
     const blogs = useSelector((state) => state.blog.blogs)
 
-    let sortedContent;
+    let content;
 
     if (sortFilter === 'oldest') {
-        sortedContent = blogs.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+        content = blogs.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
             .map((blog) =>
                 <Card key={blog._id} blog={blog} />
             )
     } else if (sortFilter === 'newest') {
-        sortedContent = blogs.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+        content = blogs.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
             .map((blog) =>
                 <Card key={blog._id} blog={blog} />
             )
@@ -30,7 +30,7 @@ const AllProducts = () => {
         <div>
             <h1 className="text-4xl text-center p-9">All Blogs</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mx-auto p-8">
-                {sortedContent}
+                {content}
             </div>
         </div>
     );
--- src/pages/main/ReadingHistory.jsx
+++ src/pages/main/ReadingHistory.jsx
@@ -1,8 +1,20 @@
+import { useSelector } from "react-redux";
+import Card from "./Card";
 
 const ReadingHistory = () => {
+    const readingHistory = useSelector((state) => state.blog.readingHistory)
+    console.log(readingHistory);
     return (
         <div>
             <h1 className="text-4xl text-center p-9">Reading History</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mx-auto p-8">
+                {
+                    readingHistory.map((item) => (
+                        <Card key={item._id} blog={item} />
+                    ))
+                }
+            </div>
         </div>
     );
 };
